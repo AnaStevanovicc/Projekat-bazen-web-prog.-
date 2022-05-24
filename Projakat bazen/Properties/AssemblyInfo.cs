b4eca6711c7903a452ce96d4a3ebbd81bbd29c97
AnaStevanovicc <ana.stevanovic@prvabeogim.edu.rs>
2022-05-24 23:23:58 +0200

    Add project files.

---
+++ Projakat bazen/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Projakat_bazen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Projakat_bazen")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a629145-2e54-4fe2-9f3f-5079f6ce3a2c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Projakat bazen/Zakazivanje_bazen.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Projakat_bazen
+{
+  
+    public class Zakazivanje_bazen
+    {
+        SqlConnection conn = new SqlConnection();
+        string wconfig = ConfigurationManager.ConnectionStrings["ProjekatBazen"].ConnectionString;
+        SqlCommand comm = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        public int Provera_Korisnika(string email, string pass)
+        {
+            conn.ConnectionString = wconfig;
+            int rezultat;
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "dbo.Korisnik_Email";
+            comm.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar, 40, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, email));
+            comm.Parameters.Add(new SqlParameter("@loz", SqlDbType.NVarChar, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, pass));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public string Provera_Korisnika(string email)
+        {
+            string istina;
+            if (email == "")
+            {
+
+                istina = "false";
+
+            }
+            else
+            {
+                istina = "true";
+            }
+            return istina;
+
+        }
+
+        public int Unos_Korisnika(string email, string pass)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Korisnik_Insert";
+         
+            comm.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar, 40, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, email));
+            comm.Parameters.Add(new SqlParameter("@loz", SqlDbType.NVarChar, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, pass));
+
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Izmeni_Korisnika(string email, string pass)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "dbo.Korisnik_Update";
+          
+            comm.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar, 40, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, email));
+            comm.Parameters.Add(new SqlParameter("@loz", SqlDbType.NVarChar, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, pass));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Bacanje_Korisnik(string email)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "dbo.Korisnik_Delete";
+          
+            comm.Parameters.Add(new SqlParameter("@email", SqlDbType.NVarChar, 40, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, email));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Unos_Lokacije(string grad)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Lokacija_Insert";
+            comm.Parameters.Add(new SqlParameter("@grad", SqlDbType.NVarChar, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, grad));
+
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Izmeni_Lokacija(int id, string grad)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "dbo.Lokacija_Update";
+   
+            comm.Parameters.Add(new SqlParameter("@id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, id));
+            comm.Parameters.Add(new SqlParameter("@grad", SqlDbType.NVarChar, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, grad));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Unos_Proizvodi(string naziv, int cena)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Proizvodi_Insert";
+            comm.Parameters.Add(new SqlParameter("@naziv", SqlDbType.NVarChar, 30, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, naziv));
+            comm.Parameters.Add(new SqlParameter("@cena", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, cena));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Izmeni_Proizvodi(string naziv, int cena)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "dbo.Prozivodi_Update";
+
+            comm.Parameters.Add(new SqlParameter("@naziv", SqlDbType.NVarChar, 30, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, naziv));
+            comm.Parameters.Add(new SqlParameter("@cena", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, cena));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Bacanje_Proizvodi(string naziv)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Proizvodi_Delete";
+            comm.Parameters.Add(new SqlParameter("@naziv", SqlDbType.NVarChar, 30, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, naziv));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Unos_Bazeni(string naziv, int lokacija_id)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Bazen_Insert";
+            comm.Parameters.Add(new SqlParameter("@naziv", SqlDbType.NVarChar, 30, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, naziv));
+            comm.Parameters.Add(new SqlParameter("@lokacija_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, lokacija_id));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Unos_Iznajmljivanje(int korisnik_id, int termin_id, string datum, int bazen_id)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Iznajmljivanje_Insert";
+            comm.Parameters.Add(new SqlParameter("@korisnik_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, korisnik_id));
+            comm.Parameters.Add(new SqlParameter("@termin_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, termin_id));
+            comm.Parameters.Add(new SqlParameter("@datum", SqlDbType.Date, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, Convert.ToDateTime(datum)));
+            comm.Parameters.Add(new SqlParameter("@bazen_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, termin_id));
+
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+
+        public int Bacanje_Iznajmljivanje(int korisnik_id, int termin_id, string datum, int bazen_id)
+        {
+
+            conn.ConnectionString = wconfig;
+            int rezultat;
+
+            comm.Connection = conn;
+            comm.CommandType = CommandType.StoredProcedure;
+            comm.CommandText = "Iznajmljivanje_Delete";
+            comm.Parameters.Add(new SqlParameter("@korisnik_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, korisnik_id));
+            comm.Parameters.Add(new SqlParameter("@termin_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, termin_id));
+            comm.Parameters.Add(new SqlParameter("@datum", SqlDbType.Date, 20, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, Convert.ToDateTime(datum)));
+            comm.Parameters.Add(new SqlParameter("@bazen_id", SqlDbType.Int, 4, ParameterDirection.Input, false, 0, 0, "", DataRowVersion.Current, termin_id));
+            comm.Parameters.Add(new SqlParameter("@RETURN_VALUE", SqlDbType.Int, 4, ParameterDirection.ReturnValue, true, 0, 0, "", DataRowVersion.Current, null));
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+
+            int Ret;
+            Ret = (int)comm.Parameters["@RETURN_VALUE"].Value;
+            if (Ret == 0)
+            {
+                rezultat = 0;
+            }
+
+            else
+            {
+                rezultat = 1;
+            }
+            return rezultat;
+        }
+    }
+}
